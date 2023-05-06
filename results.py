424d00cb9d0da7bf304df8b82e6be194c76dd4f7
Javier Serna <hipc04.javier@gmail.com>
2023-05-06 02:04:35 -0700

    Update results.py

--- results.py
+++ results.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv("Fourier.out", delimiter=' ')
-table=df.groupby('#file').median()
-df2=table[['vsini','vsini_err']]
+df = pd.read_csv("Fourier.out", delimiter=' ', comment='!')
+table=df.groupby('#file')[['vsini', 'vsini_err']].median()
+df2=table
 df2.to_csv('results.csv')
-print(df2)
+print(table)
